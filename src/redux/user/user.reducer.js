950463039c87b3a83cb4e4dbe744b911bf560ab5
Goutam A S <goutamas.keladi@gmail.com>
2020-05-01 23:33:53 +0530

    Implemented redirect and constants

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import * as ActionTypes from './user.constants';
+
 const INITIAL_STATE = {
 	currentUser: null
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
-		case 'SET_CURRENT_USER':
+		case ActionTypes.SET_CURRENT_USER:
 			return {
 				...state,
 				currentUser: action.payload
